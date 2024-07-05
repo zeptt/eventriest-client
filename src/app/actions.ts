1ae816aaae07ac35f1818d3ce176f06e47452a6b
Kushagra102 <kushagra.garg77@gmail.com>
2024-07-06 04:35:30 +0530

    Create Event, toaster

--- src/app/actions.ts
+++ src/app/actions.ts
@@ -6,12 +6,13 @@ import { cookies } from "next/headers";
 
 export async function deleteEvent(eventId: number) {
   const cookie = cookies().get("sid");
-
+  
   const res = await api.delete(`/event/${eventId}`, {
     headers: {
       cookie: `${cookie?.name}=${cookie?.value}`,
     },
   });
+
 
   revalidatePath("/dashboard");
 }
